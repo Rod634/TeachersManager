025bc35793ca019e27054f7d41922fd1a6afd28c
Rodrigo Silva <rodrigocsilva63@gmail.com>
2020-11-17 07:16:48 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
+
+const server = express();
+
+//define o tipo do template e o tipo de arquivo
+server.set("view engine", "njk");
+
+//Define um diretório onde idel para vc acessar itens de estilo
+server.use(express.static('public'));
+server.use(routes);
+
+//Caminho do arquivo, objetos e sua variavel
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true
+});
+
+//Define a porta onde o servidor irá rodar
+server.listen(5100, function(){
+    console.log("Server is running");
+});
+
